3109b4bef7cb890a3172df00a2096d44499c8fe8
Aircraft-A350 <frantisek.j.kozisek@gmail.com>
2025-05-16 18:57:04 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('.menu-icon').addEventListener('click', function() {
+    document.querySelector('.menu').style.display = 
+        document.querySelector('.menu').style.display === 'block' ? 'none' : 'block';
+});
